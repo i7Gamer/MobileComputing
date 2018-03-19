3ddb4b22cdb2040a3bd593ccb2e1a47ea7668126
i7Gamer <timorzipa@gmail.com>
2018-03-19 10:32:01 +0100

    values

--- app/src/main/java/at/fhv/mobilecomputing/Navigation.java
+++ app/src/main/java/at/fhv/mobilecomputing/Navigation.java
@@ -2,9 +2,8 @@ package at.fhv.mobilecomputing;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,10 +11,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Navigation extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    ListView shoppingList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +43,18 @@ public class Navigation extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        String[] testdata = new String[]{"Apple", "Avocado", "Banana",
+                "Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
+                "Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple"};
+
+        shoppingList = findViewById(R.id.MainShoppingList);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                testdata);
+        shoppingList.setAdapter(adapter);
+
     }
 
     @Override
